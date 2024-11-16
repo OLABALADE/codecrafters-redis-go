c145e3034d11cb9ce34c99a54964f49ef7e6fc28
Olabalade Emmanuel <olabaladeemmanuel@gmail.com>
2024-11-16 19:37:26 +0100

    Handle multiple ping

--- app/server.go
+++ app/server.go
@@ -6,26 +6,37 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-    conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+
+	conn, err := l.Accept()
+	for {
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handleClient(conn)
 	}
 
-    conn.Write([]byte("+PONG\r\n"))
+}
+func handleClient(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+        fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(buf))
+	conn.Write([]byte("+PONG\r\n"))
 }
